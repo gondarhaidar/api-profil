101f7327b25a869f79526d710e4ca6a34955abfe
gondarhaidar <gondarhaidar@gmail.com>
2025-01-18 09:26:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const port = 3000;
+const fs = require("fs");
+if (!fs.existsSync("./data.json")) {
+  fs.writeFileSync("data.json", "{}");
+}
+
+app.get("/project", (req, res) => {
+  const db = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+});
+
+app.use((req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+app.listen(port, () => {
+  console.log("API running on port " + port);
+});
